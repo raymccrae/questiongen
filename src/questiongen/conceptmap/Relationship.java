e1abf34db01a834ee9dd7a681f3e948fdae8e0d1
n601386 <n601386@e6b573eb-624a-0410-b61a-bc52bed230ce>
2015-07-26 15:07:26 +0100

    Add concept and relationship classes

---
+++ src/questiongen/conceptmap/Relationship.java
@@ -0,0 +1,44 @@
+package questiongen.conceptmap;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by raymond on 26/06/2015.
+ */
+public class Relationship {
+    private Concept sourceConcept;
+    private Concept targetConcept;
+    private String linkingTerm;
+    private Set<String> alternativeTerms;
+
+    public Relationship(Concept sourceConcept, Concept targetConcept, String linkingTerm) {
+        this.sourceConcept = sourceConcept;
+        this.targetConcept = targetConcept;
+        this.linkingTerm = linkingTerm;
+        this.alternativeTerms = new HashSet<String>();
+
+        sourceConcept.addRelationship(this);
+    }
+
+    public Concept getSourceConcept() {
+        return sourceConcept;
+    }
+
+    public Concept getTargetConcept() {
+        return targetConcept;
+    }
+
+    public String getLinkingTerm() {
+        return linkingTerm;
+    }
+
+    public Set<String> getAlternativeTerms() {
+        return Collections.unmodifiableSet(alternativeTerms);
+    }
+
+    public void addAlternativeTerm(String alternativeTerm) {
+        alternativeTerms.add(alternativeTerm);
+    }
+}
